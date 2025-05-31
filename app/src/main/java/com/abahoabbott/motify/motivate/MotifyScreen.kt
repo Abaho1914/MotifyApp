80e274e944c54c7483336e8f9d7bb92c4d2ba235
Abaho Abbott Strange <2020mbr001@std.must.ac.ug>
2025-05-31 20:47:29 +0300

    refactor: Remove unused import
    This commit removes the unused import `androidx.compose.material3.rememberSnackbarHostState` from `MotifyScreen.kt`.

--- app/src/main/java/com/abahoabbott/motify/motivate/MotifyScreen.kt
+++ app/src/main/java/com/abahoabbott/motify/motivate/MotifyScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
-//import androidx.compose.material3.rememberSnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
