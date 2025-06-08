0298058f3b35fe94287241811c3a7c5b26c61043
Abaho Abbott Strange <2020mbr001@std.must.ac.ug>
2025-06-08 09:16:59 +0300

    feat: Integrate Firebase Analytics for event tracking
    This commit introduces Firebase Analytics to the application for tracking user events.
    
    Changes include:
    - Added `AnalyticsManager.kt` to encapsulate Firebase Analytics logic. This class includes methods for logging app open events and custom events.
    - Updated `WorkerModule.kt` to provide `FirebaseAnalytics` and `AnalyticsManager` instances using Hilt.
    - Modified `NotificationManagerHelper.kt` to accept an `AnalyticsManager` dependency and log an `app_open` event when a notification is tapped.

--- app/src/main/java/com/abahoabbott/motify/di/WorkerModule.kt
+++ app/src/main/java/com/abahoabbott/motify/di/WorkerModule.kt
@@ -3,10 +3,14 @@ package com.abahoabbott.motify.di
 import android.content.Context
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.WorkManager
+import com.abahoabbott.motify.analytics.AnalyticsManager
 import com.abahoabbott.motify.notify.NotificationManagerHelper
 import com.abahoabbott.motify.qoutes.FirebaseQuoteProvider
 import com.abahoabbott.motify.qoutes.QuoteProvider
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,8 +24,8 @@ import javax.inject.Singleton
 object WorkerModule {
         @Provides
         @Singleton
-        fun provideNotificationManagerHelper(@ApplicationContext ctx: Context) =
-            NotificationManagerHelper(ctx)
+        fun provideNotificationManagerHelper(@ApplicationContext ctx: Context,analyticsManager: AnalyticsManager) =
+            NotificationManagerHelper(ctx,analyticsManager)
 
     /** Provide the platform NotificationManagerCompat instance */
     @Provides
@@ -44,6 +48,18 @@ object WorkerModule {
     @Provides
     fun provideQuoteProvider(firestore: FirebaseFirestore): QuoteProvider {
         return FirebaseQuoteProvider(firestore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAnalytics(@ApplicationContext context: Context): FirebaseAnalytics {
+        return Firebase.analytics
+    }
+
+    @Provides
+    @Singleton
+    fun provideAnalyticsManager(firebaseAnalytics: FirebaseAnalytics): AnalyticsManager {
+        return AnalyticsManager(firebaseAnalytics)
     }
 
 }
